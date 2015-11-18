6ab57faee2f54227a08ffd0c08d9e6239337ba20
18404715033 <965630447@qq.com>
2015-11-18 21:21:50 +0800

    通过光敏电阻测环境的光度
    在单板机的引脚上接入光敏电阻和一个显示器。先初始化串口 Serial.begin(9600);
    通过光敏电阻测出当前环境光度并输入

---
+++ photosensitive.ino
@@ -0,0 +1,14 @@
+void setup() 
+{
+  // put your setup code here, to run once:
+  //初始化串口
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  int sensorValue = analogRead(A0);
+  Serial.println(sensorValue);
+  delay(1000);
+}
